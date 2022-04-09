2925e2f446d0959fe838e461f0afc3c44dc19f9f
ManelTabessi <manel.tabessi@esprit.tn>
2022-04-09 13:46:17 +0200

    Initial commit

---
+++ src/test/java/com/example/springbootspringsecuritywithjwt/SpringBootSpringSecurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootspringsecuritywithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpringSecurityWithJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
